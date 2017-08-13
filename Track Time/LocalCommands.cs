057e426232aca32fcdc76bbbe8766008b4cae9d3
John Colagioia <john@colagioia.net>
2017-08-13 06:53:09 -0400

    Move using statements inside namespace

--- Track Time/LocalCommands.cs
+++ Track Time/LocalCommands.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-
 namespace Track_Time
 {
     class LocalCommands
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+
     {
         public static readonly RoutedUICommand Pause
             = new RoutedUICommand(
@@ -37,4 +37,4 @@ namespace Track_Time
                 typeof(LocalCommands)
                 );
     }
-}
+}
