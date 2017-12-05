965e57cc190a2cfbe94db36e6a94468afe24a741
John Colagioia <john@colagioia.net>
2017-12-05 06:21:51 -0500

    Move using statements

--- Track Time/App.xaml.cs
+++ Track Time/App.xaml.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-
 namespace Track_Time
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows;
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
